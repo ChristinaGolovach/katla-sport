49949f5f91f047e9bd80e06aae8ffcb5003326c7
Christina Golovach <christina.golovach@gmail.com>
2019-01-22 03:55:49 -0800

    Add implementation of onSubmit, onDelete, onUnDelete, onPurge methods in HiveFormComponent. Add Error Handler Service.

--- KatlaSportNg/src/app/hive-management/forms/hive-form.component.ts
+++ KatlaSportNg/src/app/hive-management/forms/hive-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HiveService } from '../services/hive.service';
 import { Hive } from '../models/hive';
+import { ErrorHandlerService } from '../services/error-handler.service';
 
 @Component({
   selector: 'app-hive-form',
@@ -16,13 +17,14 @@ export class HiveFormComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private hiveService: HiveService
-  ) { }
+    private hiveService: HiveService,
+    private errorService: ErrorHandlerService
+    ) { }
 
   ngOnInit() {
     this.route.params.subscribe(p => {
       if (p['id'] === undefined) return;
-      this.hiveService.getHive(p['id']).subscribe(h => this.hive = h);
+      this.hiveService.getHive(p['id']).subscribe(h => this.hive = h, error => alert(this.errorService.handleError(error)));
       this.existed = true;
     });
   }
@@ -36,14 +38,28 @@ export class HiveFormComponent implements OnInit {
   }
   
   onSubmit() {
+    if (this.existed){
+      this.hiveService.updateHive(this.hive)
+                      .subscribe(() => this.navigateToHives(), error => alert(this.errorService.handleError(error)))
+    }
+    else{
+      this.hiveService.addHive(this.hive)
+                      .subscribe(() => this.navigateToHives(), error => alert(this.errorService.handleError(error)))
+    }
   }
 
   onDelete() {
+    this.hiveService.setHiveStatus(this.hive.id, true)
+                    .subscribe(() => this.hive.isDeleted = true, error => alert(this.errorService.handleError(error)))
   }
 
   onUndelete() {
+    this.hiveService.setHiveStatus(this.hive.id, false)
+                    .subscribe(() => this.hive.isDeleted = false, error => alert(this.errorService.handleError(error)))
   }
 
   onPurge() {
+    this.hiveService.deleteHive(this.hive.id)
+                    .subscribe(() => {this.navigateToHives()}, error => alert(this.errorService.handleError(error)))
   }
 }
